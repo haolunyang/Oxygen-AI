9b61ce8b276064b9bd75365bd5448bfe34cfb65f
lifecreate <harryyhl@icloud.com>
2024-02-17 09:41:24 -0800

    Initial Commit

---
+++ Oxygen-AI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Oxygen-AI
+//
+//  Created by Haolun Yang on 2/17/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ Oxygen-AI/Oxygen_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Oxygen_AIApp.swift
+//  Oxygen-AI
+//
+//  Created by Haolun Yang on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Oxygen_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
